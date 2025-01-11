8d500dea6090e62f8c368cd2dfff0155f4ab6a7a
emkab <me@emkab.dev>
2025-01-12 01:41:53 +0200

    BROKEN

---
+++ CMakeFiles/MyCrossPlatformApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyCrossPlatformApp.dir/src/main.cpp.o"
+  "CMakeFiles/MyCrossPlatformApp.dir/src/main.cpp.o.d"
+  "MyCrossPlatformApp"
+  "MyCrossPlatformApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyCrossPlatformApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/emkab/dev/2dc2p
+# Build directory: /Users/emkab/dev/2dc2p
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(app_test "/Users/emkab/dev/2dc2p/MyCrossPlatformApp")
+set_tests_properties(app_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/emkab/dev/2dc2p/CMakeLists.txt;80;add_test;/Users/emkab/dev/2dc2p/CMakeLists.txt;0;")
